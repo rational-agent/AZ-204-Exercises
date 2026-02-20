c5274581759cdf23cf815f8a002310e837fa947b
rene.vanderhorst <rene.vanderhorst@teamrockstars.com>
2026-02-20 15:18:59 +0100

    blob exercise

---
+++ AZ-204-Exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AZ_204_Exercises;
+
+Console.WriteLine("What's your name?");
+var name = Console.ReadLine();
+
+if (name != null)
+{
+    var blob = new BlobExercise();
+    blob.ProcessAsync();
+}
